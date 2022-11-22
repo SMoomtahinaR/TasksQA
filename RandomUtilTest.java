a464c23565b1feae465cf036cec27b8b9b0e8855
Moomtahina Rahman <118716792+SMoomtahinaR@users.noreply.github.com>
2022-11-22 22:36:49 +0600

    Update RandomUtilTest.java

--- RandomUtilTest.java
+++ RandomUtilTest.java
@@ -1,18 +1,16 @@
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 public class RandomUtilTest
 {
-    @Test(dataProvider="stringy")
-    public void stringTest(int n, boolean flag)
-    {
-        System.out.println(n+""+flag);
+    @DataProvider (name = "stringy")
+    public Object[][] dpMethod(){
+        return new Object[][] {{"1,true"}, {"2,false"},{"-1,true"}};
     }
-
-    @DataProvider(name = "stringy")
-    public Object[][] getData()
-    {
-        Object[][] data = {{"3", "True"},{"3", "false"},,{"-6", "true"}};
-        return data;
+    @Test (dataProvider = "stringy")
+    public void myTest (String val) {
+        System.out.println("Passed Parameter Is : " + val);
     }
-
 }
